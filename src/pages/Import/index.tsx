282786fd0500e3623190f2ad45ee4ce8242aa835
rafaelnunesCodeGalaxy <55004937+rafaelnunesCodeGalaxy@users.noreply.github.com>
2020-05-14 08:20:05 -0300

    finish shuld be able to upload a file, shuld be able to navigate to the import page and list the total balance

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.forEach(file => {
+      const fileObject: FileProps = {
+        file,
+        name: file.name,
+        readableSize: String(file.size),
+      };
+
+      setUploadedFiles([...uploadedFiles, fileObject]);
+    });
   }
 
   return (
